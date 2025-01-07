3ad1405af03c7a3860cbd3f8d5db58c572c666e0
Park-GeunWoo <pcdpcd100@gmail.com>
2025-01-07 18:58:08 +0900

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ### Vehicle_Diagnostics_Tool
+
+pip install psutil
+
--- scripts/sys_status_monitor.py
+++ scripts/sys_status_monitor.py
@@ -2,13 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-
-def main():
-    rospy.init_node('sys_status_monitor')
-    rospy.spin()
-
+import psutil
+import subprocess
+def sys_monitor():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    core_usage = psutil.cpu_percent(interval=1, percpu=True)
+    
+    gpu_usage = subprocess.run(
+        ['nvidia-smi',
+         '--query-gpu=utilization.gpu', 
+         '--format=csv,noheader,nounits'
+         ],
+        stdout=subprocess.PIPE,
+        text=True
+    )
 if __name__ == '__main__':
     try:
-        main()
+        rospy.init_node('sys_status_monitor')
+        rospy.spin()
+
     except rospy.ROSInterruptException:
         pass
